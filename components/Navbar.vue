81d5ab5d2ee5a12d0c472c7e2514d2c522bfb597
Furkan <furkandlkdr@hotmail.com>
2024-06-14 11:55:13 +0300

    Added mobile menu icon only

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="navbar font-sans font-bold bg-base-200 justify-between fixed top-0 left-0 right-0 z-20 lg:h-auto h-20">
-        <div class="gap-x-8">
+        <div class="gap-x-8" v-if="!isMobile">
             <a class="btn btn-ghost text-xl">
                 <img src="assets/onlylogo.svg" alt="logo">
             </a>
@@ -23,6 +23,22 @@
                 <li><a>İletişim</a></li>
             </ul>
         </div>
+        <!-- Mobile menu -->
+        <div class="flex" v-else>
+            <label class="flex-start btn btn-circle swap swap-rotate">
+                <input type="checkbox">
+                <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+                    viewBox="0 0 512 512">
+                    <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+                </svg>
+                <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+                    viewBox="0 0 512 512">
+                    <polygon
+                        points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 
+                        366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                </svg>
+            </label>
+        </div>
         <!-- Language Change -->
         <div class="mx-8">
             <ul class="text-xl menu menu-horizontal px-1 gap-x-4">
@@ -43,8 +59,15 @@
 </template>
 
 <script>
+import { useMediaQuery } from '@vueuse/core';
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    setup() {
+        const isMobile = useMediaQuery('(max-width: 768px)');
+        return {
+            isMobile
+        };
+    }
 }
 </script>
 
