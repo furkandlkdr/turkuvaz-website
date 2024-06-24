1a2614ca001df380a7efe8b94a925fa8a6c00726
Furkan <furkandlkdr@hotmail.com>
2024-06-24 18:16:16 +0300

    Fixed 1 image, changed descriptions in references

---
+++ data/references.js
@@ -0,0 +1,157 @@
+export const references = [
+  {
+    name: "Altınay",
+    url: "altinay.png",
+    description: "AS/RS Sistemi, Endüstriyel Otomasyon ve 2Win Scada",
+  },
+  {
+    name: "Anka Elektrik Elektronik Otomasyon",
+    url: "anka.png",
+    description: "Scada ve Otomasyon, Kablo makinaları yazılımı,2Win Scada",
+  },
+  {
+    name: "Arçelik",
+    url: "arcelik.png",
+    description: "2Bee AS/RS Sistemi, Endüstriyel Otomasyon ve 2Win Scada",
+  },
+  {
+    name: "Bellona",
+    url: "bellona.png",
+    description: "2Mes Veri Toplama Yazılımı",
+  },
+  {
+    name: "Boyteks",
+    url: "boyteks.png",
+    description:
+      "2Bee AS/RS, Scada Yazılımı, 2Win Scada, Germe makinası otomasyon revizyonu",
+  },
+  {
+    name: "Çalık Denim",
+    url: "calik_denim.png",
+    description: "2Bee AS/RS Sistemi, Endüstriyel Otomasyon ve 2Win Scada",
+  },
+  {
+    name: "Doğtaş",
+    url: "dogtas.png",
+    description: "2Mes Veri Toplama Yazılımı",
+  },
+  {
+    name: "Form Sünger",
+    url: "form_sunger.png",
+    description: "2Mes Veri Toplama Yazılımı",
+  },
+  { name: "Gelal", url: "gelal.png", description: "AS/RS Yazlımı" },
+  {
+    name: "Güven Mühendislik Makina",
+    url: "guven_muhendislik_makina.png",
+    description:
+      "AS/RS Yazlımı, Endüstriyel Otomasyon, Otomasyon ve 2Win Scada",
+  },
+  {
+    name: "İstikbal",
+    url: "istikbal.png",
+    description:
+      "Endüstriyel Otomasyon ve 2Mes Veri Toplama Yazılımı, Otomasyon ve 2Win Scada",
+  },
+  {
+    name: "Kayseri Su ve Kanalizasyon İdaresi",
+    url: "kaski.png",
+    description: "2Win Scada, Otomasyon, Uzak Kuyu Yönetimi Sistemi",
+  },
+  {
+    name: "Kayseri Organize Sanayi Bölgesi",
+    url: "kayseri_osb.png",
+    description: "Endüstriyel Otomasyon, Briket Üretim Hattı ve Paketleme",
+  },
+  {
+    name: "Kayseri Ulaşım",
+    url: "kayseri_ulasim.png",
+    description: "Özel Yazılım, Kaybis Yazılımı",
+  },
+  {
+    name: "Kelebek",
+    url: "kelebek.png",
+    description: "2Mes Veri Toplama Yazılımı",
+  },
+  {
+    name: "Lineer Makina",
+    url: "lineer_makina.png",
+    description: "Endüstriyel Otomasyon, Sıvı Dolum Makinası plc programı",
+  },
+  {
+    name: "Melikgazi Belediyesi",
+    url: "melikgazi_belediyesi.png",
+    description: "T2Kuyu, Uzak Kuyu Yönetim Yazılımı",
+  },
+  {
+    name: "Mesa",
+    url: "mesa.png",
+    description: "Gömülü Sistemler, RF Kumanda Kartları.",
+  },
+  {
+    name: "Mondi",
+    url: "mondi.png",
+    description: "2Mes Veri Toplama Yazılımı",
+  },
+  {
+    name: "NKS Kablo",
+    url: "nks_kablo.png",
+    description:
+      "Endüstriyel Otomasyon ve 2Win Scada, Foam Skin Makinası otomasyon revizyonu",
+  },
+  {
+    name: "Nurol Teknoloji",
+    url: "nurol_teknoloji.png",
+    description: "Endüstriyel Otomasyon, Vakumlu Fırın Otomasyonu, 2Win Scada",
+  },
+  {
+    name: "Orta Anadolu",
+    url: "orta_anadolu.png",
+    description:
+      "Endüstriyel Otomasyon, 2Win Scada, Enerji Otomasyon ve Scada Yazılımı, Su Hazırlama Birimi Otomasyon ve Scada Yazılımı, Sanfor Makinası Otomasyon Revizyonu",
+  },
+  {
+    name: "Ravago",
+    url: "ravago.png",
+    description:
+      "Endüstriyel Otomasyon, 2Win Scada, Taş Yünü Hattı Otomasyonu ve Scada Yazılımı, Reçine Reaktörü Scada Yazılımı, Pres Paketleme Otomasyonu",
+  },
+  {
+    name: "Simfer",
+    url: "simfer.png",
+    description:
+      "Endüstriyel Otomasyon, 2Win Scada, Montaj Bandları Otomasyon ve Scada Yazılımı",
+  },
+  {
+    name: "Şirikçioğlu Mensucat",
+    url: "sirikcioglu.png",
+    description:
+      "Endüstriyel Otomasyon, 2Win Scada, Indigo Otomasyon ve Scada Revizyonu",
+  },
+  {
+    name: "SMG Makina",
+    url: "smg_makina.png",
+    description: "Endüstriyel Otomasyon, Gazlı Amortisör Montaj Hattı",
+  },
+  {
+    name: "Turkish Aerospace Industries",
+    url: "tai.png",
+    description:
+      "Veri Toplama ve Özel Scada Yazılımı, Scada ve Veri Toplama Yazılımı ile Plc yazılımı, Ultrasonik kompozit malzeme tarama projesi bilgisayar yazılımı",
+  },
+  {
+    name: "Talas Belediyesi",
+    url: "talas_belediyesi.png",
+    description: "İş Takip Yazılımı",
+  },
+  {
+    name: "Türk Hava Yolları Teknik A.Ş.",
+    url: "turkish_technic.png",
+    description: "2Bee AS/RS Sistemi",
+  },
+  {
+    name: "Vibroblok",
+    url: "vibroblok.png",
+    description: "Scada ve Otomasyon, 2Win Scada",
+  },
+];
---
+++ pages/references.vue
@@ -0,0 +1,42 @@
+<template>
+    <title> Referanslar </title>
+    <div>
+        <div class="h-24">
+            <Navbar />
+        </div>
+        <div class="grid grid-cols-2 xl:grid-cols-4 w-11/12 2xl:w-9/12 mx-auto gap-2 xl:gap-6">
+            <div v-for="company in references" :key="company.url" class="card bg-neutral-content shadow-lg">
+                <div class="2xl:h-52 h-24 flex items-center justify-center mb-2">
+                    <figure class="w-full h-full flex items-center justify-center mx-4 mt-2">
+                        <NuxtImg :src="`images/${company.url}`" :alt="`${company.name} logo`"
+                            class="w-full h-full object-contain" />
+                    </figure>
+                </div>
+                <div class="items-center text-center">
+                    <h2 class="text-2xl font-bold colorize-font">{{ company.name }}</h2>
+                    <p class="text-xl font-medium colorize-font mt-2 mb-6 mx-4">{{ company.description }}</p>
+                </div>
+            </div>
+        </div>
+
+        <Footer class="mt-8"/>
+    </div>
+</template>
+
+<script>
+import { references } from '~/data/references.js';
+export default {
+    name: 'References',
+    setup() {
+        return {
+            references,
+        };
+    }
+}
+</script>
+
+<style scoped>
+.colorize-font {
+    color: oklch(var(--n));
+}
+</style>
