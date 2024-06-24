c38614f87e8c1e00a59262309d523b681d35b375
Furkan <furkandlkdr@hotmail.com>
2024-06-24 12:03:50 +0300

    Added static services page and made a page for organize.

---
+++ data/services.js
@@ -0,0 +1,112 @@
+export const services = [
+  {
+    id: 1,
+    systemName: "2Win",
+    title: "Scada Sistemleri",
+    descriptions: [
+      {
+        isList: false,
+        text: `SCADA terimi Supervisory Control and Data Acquisition kelimelerinin ilk harfleri ile oluşturulan bir kısaltmadır. 
+Kapsamlı ve entegre bir Veri Tabanlı Kontrol ve Gözetleme Sistemi (Supervisory Control and Data Acquisition (SCADA) ) kontrol sistemi sayesinde, 
+bir tesise veya işletmeye ait tüm ekipmanların kontrolünden üretim planlamasına, çevre kontrol ünitelerinden yardımcı işletmelere kadar 
+tüm birimlerin otomatik kontrolü ve gözlenmesi sağlanabilir. Anlık olay ve alarmları saklayarak geçmişte meydana gelen olayları da 
+tekrar günün tarihinde ve saatinde gözlemleyebilmemizide sağlayan geniş kapsamlı bir sistemdir.`,
+      },
+      {
+        isList: false,
+        text: `Rekabet koşullarının her geçen gün artması, teknolojik gelişmeler ve artan müşteri bilinci işletmeleri aynı anda 
+farklı boyutlarda yarışmaya zorlamaktadır. Kalitede yaşanan rekabet hizmet kalitesi ve güvenirliliğine önem verilmesini zorunlu kılmaktadır. 
+Ürün fiyatlarında yaşanan rekabet ise işletmelerin maliyetlere odaklanmasını gerektirir. 
+Maliyetler düşünüldüğünde zamanın ve insan kaynaklarının etkin kullanımı akla gelir. Burada anahtar kelime ölçmedir. 
+İşletmeye ait tüm çalışma verilerinin ölçülmesi ve bu ölçümler sonucunda yapılacak değişiklikler işletmeye gereken rekabet gücünü sağlayacaktır. 
+SCADA yazılımları işletmeye bu ölçme verilerini sağlayacak ve aynı zamanda sahaya yapılacak anlık müdahalelere imkan tanıması ile zamanın etkin 
+kullanımını gerçekleştirecektir.`,
+      },
+      {
+        isList: false,
+        text: `Ülkemizde kullanılan scada yazılımlarının çoğu yurt dışından ithal edilmektedir. Firmamız sektördeki scada yazılımı ihtiyacını geliştirilen 
+yerli yazılımlarla karşılamayı amaçlamıştır. Firmamız bünyesinde geliştirilen scada yazılımlarında erişilebilirlik, 
+kullanım kolaylığı ve zengin animasyon seçenekleri gibi özellikler öne çıkmaktadır.`,
+      },
+    ],
+    images: [
+      "/services/2Win1.jpg",
+      "/services/2Win2.jpg",
+      "/services/2Win3.jpg",
+      "/services/2Win4.jpg",
+    ],
+  },
+  {
+    id: 2,
+    systemName: "2Das",
+    title: "Veri Toplama Sistemleri",
+    descriptions: [
+      {
+        isList: false,
+        text: `Çağımızda ürettiğiniz bir ürünün kaliteli olması, aynı türden üretim yapan rakip firmalardan ayrılmanızı sağlamaz. 
+        Üreticilerin, kaliteli ürünler üretebilmeleri adına, hammadde fiyatları artarken hem maliyetleri düşürüp hem de üretkenliği artırmak için
+        sürekli baskı altında olacakları bilinen bir gerçektir. Fiyat ve sipariş karşılama hızı bakımından rekabet edebilir durumda olmak
+        müşteri memnuniyetini sağlamak için iki anahtar noktadır. Gerçek zamanlı üretim yönetimi ara ve üst yöneticilerin kendi stratejilerini 
+        destekleyen daha doğru kararlar vermelerine olanak tanır. Örneğin, müşteri taleplerine zamanında yanıt vermek, 
+        işin hangi aşamada olduğunu ve ne zaman tamamlanacağını tam olarak belirlemek için, üretilmekte olan ürünün o ana kadarki izlenebilirliğini 
+        gerektirir. Kar oranları düşmeye devam ederken, işçilik maliyetlerini belirlemek üreticiler için kritik bir konu haline gelmektedir. 
+        Veri Toplama, Görüntüleme ve Yönetim Sistemi, üretimle ilgili kritik verileri gerçek zamanlı olarak alınmasını sağlayarak yöneticilerin 
+        en güncel verilerle karar vermelerini sağlar.`,
+      },
+      {
+        isList: false,
+        text: `Geliştirilen 2Das Veri Toplama Yazılımı, Üretim Modülü, Duruş Modülü, Arıza Bildirim ve Arıza Bakım Modülü olmak üzere 4 kısımdan oluşmaktadır.`,
+      },
+    ],
+    images: [
+      "/services/2Das1.jpg",
+      "/services/2Das2.jpg",
+      "/services/2Das3.jpg",
+      "/services/2Das4.jpg",
+    ],
+  },
+  {
+    id: 3,
+    systemName: "2Bee",
+    title: "AS/RS Sistemleri",
+    descriptions: [
+      {
+        isList: false,
+        text: `Otomatik depo sistemleri genellikle büyük hacimli ve ağır yüklerin depolanmasında, alanların yetersiz olması nedeniyle 
+          depolama yoğunluğunun yüksek olduğu durumlarda tercih edilmektedir. Üretim firmalarında ve dağıtım merkezlerinde geniş bir uygulama alanı vardır. 
+          Sistemler çoğunlukla tam otomatik olarak ve insansız çalışmaktadır. Bunun sonucunda da yüksek bir depo hassasiyeti yakalanır.`,
+      },
+
+      {
+        isList: true,
+        text: `*2Bee AS/RS Avantajları
+          *Hızlı ve doğru sevkiyat : Ürün doğrudan işlem yapılacağı noktaya getirilir. Yürüme, bekleme, arama zamanlarından tasarruf sağlanır.
+          *Siparişin işlenmesi ile ilgili sürelerde 3-5 kat daha yüksek hız yakalanır. Teslim süresi ile ilgili taahhütlerde gecikmeler yaşanmaz.
+          *Depo alanının dikey değerlendirilmesi sonucunda depolama alanları en alt seviyeye iner.
+          *Ara stoklarda azalma : Ürün ara stokları azaltılır. Yüksek depolama hassasiyeti ve üst düzey depo yönetimi ile aşırı stok önlenir.
+          *Gerçek zamanlı stok kontrolü : %100 doğruluk seviyesinde gerçek zamanlı stok kontrolü yapılır, anlık raporlama ile mevcut durum bilgilerine 
+          *her zaman ulaşmak mümkündür.
+          *Personel performansının gerçek değerler ile tespit etmek,
+          *Malzeme taşıma maliyetlerinde azalma
+          *Ekipman bakım ve işletme maliyetlerinde azalma`,
+      },
+
+      {
+        isList: true,
+        text: `*AS/RS Kullanım Alanları
+          *İlaç sektörü
+          *Lojistik
+          *Dağıtım depoları
+          *Hava kargo işletmeleri
+          *Beyaz eşya / elektronik ürün depolama
+          *Otomotiv parçaları`,
+      },
+    ],
+    images: [
+      "/services/2Bee1.jpg",
+      "/services/2Bee2.jpg",
+      "/services/2Bee3.jpg",
+      "/services/2Bee4.jpg",
+    ],
+  },
+];
---
+++ pages/services/[id].vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- Navbar -->
+    <div class="h-12">
+      <Navbar />
+    </div>
+    <!-- Page content -->
+    <div class="container mx-auto my-10">
+      <div class="flex">
+        <div class="w-1/3">
+          <h1 class="text-3xl font-bold">{{service.systemName + " " + service.title }}</h1>
+          <!-- TODO: Add a carusel or image gallery -->
+          <div v-for="image in service.images" :key="id">
+            <NuxtImg :src="image" alt="" class="mt-4 w-full" />
+          </div>
+        </div>
+        <div class="w-2/3 pl-8">
+          <div v-for="desc in service.descriptions">
+            <ol class="text-xl my-8" v-if="desc.isList">
+              <li class="my-1" v-for="item in parseList(desc.text)">{{ item }}</li>
+            </ol>
+            <p class="text-xl my-8" v-else>{{ desc.text }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+import { services } from '~/data/services.js';
+
+const route = useRoute();
+const router = useRouter();
+const serviceId = route.params.id;
+const service = services.find(service => service.id === parseInt(serviceId));
+
+if (!service) {
+  router.push('/'); // Redirect to home page if service not found
+}
+
+const parseList = (list) => {
+  list = list.split('*').map(item => item.trim()).filter(item => item !== '');
+  return list;
+}
+</script>
+
+<style scoped>
+</style>
