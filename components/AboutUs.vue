18430b7960ca683a8c09bf03670165718668023a
Furkan Dulkadir <221030910016@kayseri.edu.tr>
2024-06-26 18:17:35 +0300

    Fixed some responsive problems

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -1,49 +1,48 @@
 <template>
-    <div class="flex max-lg:flex-col mx-auto w-9/12">
-        <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-4 xl:gap-8 w-full lg:w-1/2 items-center">
+    <div class="flex max-lg:flex-col mx-auto w-11/12 lg:w-9/12">
+        <!-- Cards Box -->
+        <div class="grid grid-cols-1 xl:grid-cols-2 
+        lg:gap-4 2xl:gap-8 
+        w-full xl:w-1/2 items-center">
             <!-- Card 1 - 2 -->
             <div class="flex flex-col gap-4">
                 <!-- Card 1 -->
-                <div class="card bg-base-200 rounded-lg shadow-md p-6">
+                <div class="card bg-base-200 shadow-md p-6">
                     <h2 class="text-2xl font-bold mb-4">Otomasyon Hakkında Her Şey</h2>
-                    <p class="text-lg">Elektrik Panosu Dahil Otomasyon Sistemleri</p>
-                    <p class="text-lg">Scada Sistemleri</p>
-                    <p class="text-lg">Veri Toplama Sistemleri</p>
-                    <p class="text-lg">Otomatik Depolama Sistemleri</p>
+                    <p class="text-lg">Elektrik Panosu Dahil Otomasyon Sistemleri</p> <br>
+                    <p class="text-lg">Scada Sistemleri</p> <br>
+                    <p class="text-lg">Veri Toplama Sistemleri</p> <br>
+                    <p class="text-lg">Otomatik Depolama Sistemleri</p> <br>
                 </div>
                 <!-- Card 2 -->
-                <div class="card bg-base-200 rounded-lg shadow-md p-6">
+                <div class="card bg-base-200 shadow-md p-6">
                     <h2 class="text-2xl font-bold mb-4">Yenilikçi ve Profesyonel Çözümler</h2>
                     <p class="text-lg">İhtiyaçlarınıza Özel Çözümler</p>
                 </div>
             </div>
 
             <!-- Card 3 -->
-            <div class="flex max-lg:flex-col gap-4 max-lg:mt-8">
-                <div class="card bg-base-200 rounded-lg shadow-md p-6">
-                    <h2 class="text-2xl font-bold mb-4">Ana İlkelerimiz:</h2>
-                    <ul class="text-xl">
-                        <li>* Müşterilerimize son teknolojiyi ekonomik bir biçimde sunmak</li> <br>
-                        <li>* Mümkün olan en iyi kullanıcı deneyimini sağlamak</li> <br>
-                        <li>* Yeni teknolojilerin geliştirilmesi için arge çalışmaları yapmak</li> <br>
-                        <li>* Müşterilerimizin isteklerini kaliteden ödün vermeden gerçekleştirmek</li>
-                    </ul>
-                </div>
+            <div class="card bg-base-200 shadow-md p-6 flex max-lg:mt-8">
+                <h2 class="text-2xl font-bold mb-4">Ana İlkelerimiz:</h2>
+                <ul class="text-xl">
+                    <li>Müşterilerimize son teknolojiyi ekonomik bir biçimde sunmak</li> <br>
+                    <li>Mümkün olan en iyi kullanıcı deneyimini sağlamak</li> <br>
+                    <li>Yeni teknolojilerin geliştirilmesi için arge çalışmaları yapmak</li> <br>
+                    <li>Müşterilerimizin isteklerini kaliteden ödün vermeden gerçekleştirmek</li>
+                </ul>
             </div>
         </div>
         <!-- Info -->
-        <div class="flex flex-col lg:mx-8 w-full lg:w-1/2 max-lg:mt-4">
-            <h2 class="text-2xl font-bold mb-4">Turkuvaz İnovasyon</h2>
-            <p class="text-lg">Turkuvaz İnovasyon endüstriyel otomasyon sektöründe, bilgisayar ve PLC yazılımları
+        <div class="flex flex-col lg:mx-8 w-full xl:w-1/2 max-lg:mt-4 gap-4">
+            <h2 class="text-2xl font-bold max-xl:text-center">Turkuvaz İnovasyon</h2>
+            <p class="text-lg max-xl:text-center">Turkuvaz İnovasyon endüstriyel otomasyon sektöründe, bilgisayar ve PLC yazılımları
                 konusunda hizmet
                 vermek amacıyla 2011 yılında kurulmuştur. Firmamız AS/RS (Otomatik Yükleme / Boşaltma Sistemleri), Scada
                 Sistemleri, Veri Toplama Sistemleri gibi başlıca konularda geliştirdiğimiz otomasyon projeleri ve
                 yazılımları ile sektörde yerini almıştır. Bu temel konular yanında müşteri ihtiyacına göre özel çözümler
                 de geliştirebilmekteyiz.</p>
-            <br>
-            <p class="text-lg">Bilgisayar ve elektronik mühendislerinden oluşan tecrübeli ekibimiz, sizin için en uygun
-                çözümleri
-                üretmeyi amaç edinmiştir.</p>
+            <p class="text-lg max-xl:text-center">Bilgisayar ve elektronik mühendislerinden oluşan tecrübeli ekibimiz, sizin için en uygun
+                çözümleri üretmeyi amaç edinmiştir.</p>
         </div>
     </div>
 </template>
@@ -53,7 +52,3 @@ export default {
     name: 'AboutUs',
 }
 </script>
-
-<style>
-/* Add any custom styles here */
-</style>
--- components/Contact.vue
+++ components/Contact.vue
@@ -1,39 +1,39 @@
 <template>
     <div>
-        <div class="flex flex-col max-lg:flex-col w-9/12 mx-auto my-4">
+        <div class="flex flex-col w-11/12 lg:w-9/12 mx-auto">
             <!-- Contact Form -->
             <div class="w-full gap-6 p-6 bg-base-200 rounded-lg shadow-lg ">
-                <h1 class="text-3xl font-bold mb-4 mx-10">Bize Ulaşın</h1>
-                <p class="mx-10">Aşağıdaki formu kullanarak bize ulaşabilirsiniz</p>
-                <form class="mx-8">
+                <h1 class="text-3xl font-bold mb-4 mx-4 lg:mx-8">Bize Ulaşın</h1>
+                <p class="mx-4 lg:mx-8">Aşağıdaki formu kullanarak bize ulaşabilirsiniz</p>
+                <form @submit="sendMessage" class="mx-4 lg:mx-8">
                     <div class="flex max-lg:flex-wrap gap-8">
                         <div class="lg:w-1/3 w-full">
                             <label for="name" class="label">İsim:</label>
                             <input type="text" id="name" v-model="name" required
-                                class="input input-bordered input-primary input-lg w-full">
+                                class="input input-bordered input-info input-lg w-full">
                         </div>
                         <div class="lg:w-1/3 w-full">
                             <label for="email" class="label">E-posta:</label>
                             <input type="email" id="email" v-model="email" required
-                                class="input input-bordered input-primary input-lg w-full">
+                                class="input input-bordered input-info input-lg w-full">
                         </div>
                         <div class="lg:w-1/3 w-full">
                             <label for="phone" class="label">Telefon:</label>
                             <input type="tel" id="phone" v-model="phone" required
-                                class="input input-bordered input-primary input-lg w-full">
+                                class="input input-bordered input-info input-lg w-full">
                         </div>
                     </div>
                     <div class="w-full flex flex-col">
                         <label for="message" class="label">Mesaj:</label>
                         <textarea id="message" v-model="message" required
-                            class="textarea textarea-primary input-lg w-full min-h-60 mb-4"></textarea>
+                            class="textarea textarea-info input-lg w-full min-h-60 mb-4"></textarea>
                     </div>
                     <button type="submit" class="btn btn-success max-lg:w-full text-gray-50 ">Gönder</button>
                 </form>
             </div>
             <!-- Info text -->
-            <div class="w-full h-auto flex max-lg:flex-col justify-center items-center gap-6">
-                <div class="lg:w-1/2 h-64 flex flex-col items-center justify-center">
+            <div class="flex max-lg:flex-col justify-center items-center gap-6 lg:mt-16 mt-8">
+                <div class="lg:w-1/2 flex flex-col items-center justify-center">
                     <h1 class="card-title text-3xl text-center">Turkuvaz İnovasyon</h1>
                     <h2 class="text-xl mb-2">Sistem Çözümleri A.Ş.</h2>
                     <p class="text-center">Erciyes Üniversitesi Teknoloji Geliştirme Bölgesi</p>
@@ -64,13 +64,12 @@
             </div>
         </div>
         <!-- Map -->
-        <div class="lg:w-9/12 w-11/12 mx-auto mb-4">
+        <div class="mx-auto mt-8 lg:mt-16">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6226.292771129312!2d35.532266!3d38.714448!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x152b128d0e1746b5%3A0xe6e15ee6ff8f985b!2zVHVya3V2YXogxLBub3Zhc3lvbiBTaXN0ZW0gw4fDtnrDvG1sZXJpIEEuxZ4u!5e0!3m2!1str!2sus!4v1718009292203!5m2!1str!2sus"
                 allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                class="w-11/12 my-8 h-96 mx-auto"></iframe>
+                class="w-11/12 lg:w-9/12 h-96 mx-auto"></iframe>
         </div>
-
     </div>
 </template>
 
@@ -83,11 +82,25 @@ export default {
         const email = ref('');
         const phone = ref('');
         const message = ref('');
+
+        const sendMessage = (e) => {
+            e.preventDefault();
+            const data = {
+                name: name.value,
+                email: email.value,
+                phone: phone.value,
+                message: message.value,
+            };
+            console.log(data);
+            // TODO: Send mail to info@turkuvazinovasyon.com
+        };
+
         return {
             name,
             email,
             phone,
-            message
+            message,
+            sendMessage,
         };
     }
 };
