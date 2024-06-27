70415c4947b72ce6db96a4c6e23d09ca6f451190
Furkan Dulkadir <221030910016@kayseri.edu.tr>
2024-06-27 18:29:25 +0300

    Feat: Footer is responsive now

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-  <footer class="bg-base-200 text-base-content py-10">
-    <div class="container mx-auto flex flex-col md:flex-row justify-between items-start gap-10 md:gap-1 ">
+  <footer class="bg-base-200 text-base-content">
+    <div class="flex flex-col md:flex-row gap-10 lg:gap-1 p-6 mx-auto">
       <!-- Company Info -->
-      <div class="w-full md:mr-auto md:w-2/6">
+      <div class="w-full md:mr-auto max-md:text-center lg:w-2/6">
         <h2 class="font-bold text-lg mb-2">Turkuvaz İnovasyon</h2>
         <p>
           Tecrübeli kadrosuyla ekonomik ve yenilikçi endüstriyel çözümler sunar. Scada, veri toplama yazılımı ve
@@ -10,7 +10,7 @@
         </p>
       </div>
       <!-- Menu 1 -->
-      <div class="w-full md:w-1/6">
+      <div class="w-full max-lg:text-center lg:w-1/6">
         <h2 class="font-bold text-lg mb-2">Hizmetlerimiz</h2>
         <ul class="space-y-2">
           <li><a href="#" class="link link-hover">AS/RS Sistemleri</a></li>
@@ -21,7 +21,7 @@
         </ul>
       </div>
       <!-- Menu 2 -->
-      <div class="w-full md:w-1/6">
+      <div class="w-full max-lg:text-center lg:w-1/6">
         <h2 class="font-bold text-lg mb-2">Bağlantılar</h2>
         <ul class="space-y-2">
           <li><a href="#" class="link link-hover">Anasayfa</a></li>
